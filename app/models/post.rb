968ddbfd48d061f922ba8602303fad3a6f96e5aa
Aspet Amirkhanian <aspeta@yahoo.com>
2014-04-27 18:15:13 +0000

    Added image to Post.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ActiveRecord::Base
   belongs_to :topic
 
   default_scope { order('created_at DESC') }
-
+  mount_uploader :image, ImageUploader  
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
